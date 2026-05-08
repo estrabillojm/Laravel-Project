85bc7a3039826c6d5ee5ed22371460ed3c61f069
John Michael Estrabillo <estrabillo.johnmichael@gmail.com>
2026-05-08 22:19:40 +0800

    chore: Project Initialization

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to <b>Laravel</b></h1>
+    <p>Click the button below to view the list of Ninjas</p>
+    <a>View Ninjas</a>
+</body>
+</html>
