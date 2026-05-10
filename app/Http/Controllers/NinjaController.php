037eb741f4fd217ee3da103d20aa3dc679666a0b
John Michael Estrabillo <estrabillo.johnmichael@gmail.com>
2026-05-10 17:06:14 +0800

    chore: add ninja controller

---
+++ app/Http/Controllers/NinjaController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Ninja;
+
+class NinjaController extends Controller
+{
+    public function index(){
+        $ninjas = Ninja::orderBy('created_at', 'desc')->paginate(10);
+
+        return view('ninjas.index', ["greeting" => "Hello Ninjas", "ninjas" => $ninjas]);
+    }
+
+    public function show($id){
+        $ninja = Ninja::findOrFail($id);
+        return view('ninjas.show', ['ninja' => $ninja]);
+    }
+
+    public function create(){
+        return view('ninjas.create');
+    }
+
+    public function store(){
+
+    }
+
+    public function destroy(){
+
+    }
+}
